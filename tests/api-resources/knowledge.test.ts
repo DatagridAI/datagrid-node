a4e0b26e2571f1acb7ea83dc6ff62ee6a7557b25
andrew_zav <azavhorodnii@amcbridge.com>
2025-01-24 15:00:47 +0200

    fix: skip

--- tests/api-resources/knowledge.test.ts
+++ tests/api-resources/knowledge.test.ts
@@ -1,6 +1,6 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-import Datagrid from 'datagrid-ai';
+import Datagrid, { toFile } from 'datagrid-ai';
 import { Response } from 'node-fetch';
 
 const client = new Datagrid({
@@ -10,25 +10,25 @@ const client = new Datagrid({
 
 describe('resource knowledge', () => {
   // There is a trouble on the Prism mock side with validation for the array of files.
-  // test('create: only required params', async () => {
-  //   const responsePromise = client.knowledge.create({
-  //     files: [await toFile(Buffer.from('# my file contents'), 'README.md')],
-  //   });
-  //   const rawResponse = await responsePromise.asResponse();
-  //   expect(rawResponse).toBeInstanceOf(Response);
-  //   const response = await responsePromise;
-  //   expect(response).not.toBeInstanceOf(Response);
-  //   const dataAndResponse = await responsePromise.withResponse();
-  //   expect(dataAndResponse.data).toBe(response);
-  //   expect(dataAndResponse.response).toBe(rawResponse);
-  // });
-
-  // test('create: required and optional params', async () => {
-  //   const response = await client.knowledge.create({
-  //     files: [await toFile(Buffer.from('# my file contents'), 'README.md')],
-  //     name: 'name',
-  //   });
-  // });
+  test.skip('create: only required params', async () => {
+    const responsePromise = client.knowledge.create({
+      files: [await toFile(Buffer.from('# my file contents'), 'README.md')],
+    });
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+  // There is a trouble on the Prism mock side with validation for the array of files.
+  test.skip('create: required and optional params', async () => {
+    const response = await client.knowledge.create({
+      files: [await toFile(Buffer.from('# my file contents'), 'README.md')],
+      name: 'name',
+    });
+  });
 
   test('retrieve', async () => {
     const responsePromise = client.knowledge.retrieve('knowledge_id');
